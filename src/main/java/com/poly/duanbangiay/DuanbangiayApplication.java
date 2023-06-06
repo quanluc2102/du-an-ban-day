6922bb0ebe78d87baf44159cbf521a711cdaf791
DESKTOP-S9JDIQ2\thuct <Vietttph20936@fpt.edu.vn>
2023-06-06 18:44:28 +0700

    fix bug cant started, thêm application.properties

--- src/main/java/com/poly/duanbangiay/DuanbangiayApplication.java
+++ src/main/java/com/poly/duanbangiay/DuanbangiayApplication.java
@@ -2,8 +2,10 @@ package com.poly.duanbangiay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DuanbangiayApplication {
 
     public static void main(String[] args) {

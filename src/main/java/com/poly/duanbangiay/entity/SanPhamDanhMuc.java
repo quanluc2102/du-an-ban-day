e7642ac427bc353206c198fb9b31aa137205ce69
DESKTOP-IRM5A05\admin <ngocbe498@gmail.com>
2023-06-10 14:56:03 +0700

    Mappping sanpham,sanphamanh , sanpham danh muc

--- src/main/java/com/poly/duanbangiay/entity/SanPhamDanhMuc.java
+++ src/main/java/com/poly/duanbangiay/entity/SanPhamDanhMuc.java
@@ -1,57 +1,32 @@
 package com.poly.duanbangiay.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
 @Entity
-@Table(name = "san_pham_danh_muc", schema = "dbo", catalog = "ShopShoe")
+@Table(name = "san_pham_danh_muc")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SanPhamDanhMuc {
-    private long id;
-    private Long sanPhamId;
-    private Long danhMucId;
-
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    public long getId() {
-        return id;
-    }
+    private long id;
 
-    public void setId(long id) {
-        this.id = id;
-    }
+    @ManyToOne
+    @JoinColumn(name = "san_pham_id", referencedColumnName = "id", nullable = true)
+    private SanPham sanPham;
 
-    @Basic
-    @Column(name = "san_pham_id", nullable = true)
-    public Long getSanPhamId() {
-        return sanPhamId;
-    }
+    @ManyToOne
+    @JoinColumn(name = "danh_muc_id", referencedColumnName = "id", nullable = true)
+    private DanhMuc danhMuc;
 
-    public void setSanPhamId(Long sanPhamId) {
-        this.sanPhamId = sanPhamId;
-    }
 
-    @Basic
-    @Column(name = "danh_muc_id", nullable = true)
-    public Long getDanhMucId() {
-        return danhMucId;
-    }
-
-    public void setDanhMucId(Long danhMucId) {
-        this.danhMucId = danhMucId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SanPhamDanhMuc that = (SanPhamDanhMuc) o;
-        return id == that.id && Objects.equals(sanPhamId, that.sanPhamId) && Objects.equals(danhMucId, that.danhMucId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, sanPhamId, danhMucId);
-    }
 }

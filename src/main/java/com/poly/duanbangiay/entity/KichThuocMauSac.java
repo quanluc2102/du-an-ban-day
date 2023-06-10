a9b55f7d5438846eca932e2897eb36d0ace22733
quanluc2102 <quanluc2102@gmail.com>
2023-06-10 18:39:07 +0700

    push code hộ Giang

--- src/main/java/com/poly/duanbangiay/entity/KichThuocMauSac.java
+++ src/main/java/com/poly/duanbangiay/entity/KichThuocMauSac.java
@@ -1,22 +1,58 @@
 package com.poly.duanbangiay.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "kich_thuoc_mau_sac", schema = "dbo", catalog = "ShopShoe")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class KichThuocMauSac {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
     private long id;
+    @Column(name = "so_luong")
     private int soLuong;
+    @Column(name = "trang_thai")
     private int trangThai;
     private Long mauSacId;
     private Long sanPhamId;
     private Long kichThuocId;
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id
-    @Column(name = "id", nullable = false)
+    @ManyToOne()
+    @JoinColumn(
+            name = "mau_sac_id",
+            referencedColumnName = "id",
+            nullable = true
+    )
+
+    private MauSac mauSac;
+    @ManyToOne()
+    @JoinColumn(
+            name = "san_pham_id",
+            referencedColumnName = "id",
+            nullable = true
+    )
+
+    private SanPham sanPham;
+    @ManyToOne()
+    @JoinColumn(
+            name = "kich_thuoc_id",
+            referencedColumnName = "id",
+            nullable = true
+    )
+
+    private KichThuoc kichThuoc;
+
     public long getId() {
         return id;
     }

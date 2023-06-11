d88cfdc1862fb1a9221bb739dd7699b769a88a24
Mukkkkkkkkkkkk <105185017+Mukkkkkkkkkkkk@users.noreply.github.com>
2023-06-11 11:32:41 +0700

    repo giohangct hd hdct

---
+++ src/main/java/com/poly/duanbangiay/repository/HoaDonChiTietRepository.java
@@ -0,0 +1,7 @@
+package com.poly.duanbangiay.repository;
+
+import com.poly.duanbangiay.entity.HoaDonChiTiet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet,Long> {
+}

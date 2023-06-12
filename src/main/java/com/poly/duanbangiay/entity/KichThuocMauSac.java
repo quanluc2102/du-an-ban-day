9ded6c161838ce78cfd48b6954ea73a8e8165789
quanluc2102 <quanluc2102@gmail.com>
2023-06-12 18:03:18 +0700

    update lại của giang

--- src/main/java/com/poly/duanbangiay/entity/KichThuocMauSac.java
+++ src/main/java/com/poly/duanbangiay/entity/KichThuocMauSac.java
@@ -24,9 +24,6 @@ public class KichThuocMauSac {
     private int soLuong;
     @Column(name = "trang_thai")
     private int trangThai;
-    private Long mauSacId;
-    private Long sanPhamId;
-    private Long kichThuocId;
 
     @ManyToOne()
     @JoinColumn(
@@ -52,75 +49,4 @@ public class KichThuocMauSac {
     )
 
     private KichThuoc kichThuoc;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "so_luong", nullable = false)
-    public int getSoLuong() {
-        return soLuong;
-    }
-
-    public void setSoLuong(int soLuong) {
-        this.soLuong = soLuong;
-    }
-
-    @Basic
-    @Column(name = "trang_thai", nullable = false)
-    public int getTrangThai() {
-        return trangThai;
-    }
-
-    public void setTrangThai(int trangThai) {
-        this.trangThai = trangThai;
-    }
-
-    @Basic
-    @Column(name = "mau_sac_id", nullable = true)
-    public Long getMauSacId() {
-        return mauSacId;
-    }
-
-    public void setMauSacId(Long mauSacId) {
-        this.mauSacId = mauSacId;
-    }
-
-    @Basic
-    @Column(name = "san_pham_id", nullable = true)
-    public Long getSanPhamId() {
-        return sanPhamId;
-    }
-
-    public void setSanPhamId(Long sanPhamId) {
-        this.sanPhamId = sanPhamId;
-    }
-
-    @Basic
-    @Column(name = "kich_thuoc_id", nullable = true)
-    public Long getKichThuocId() {
-        return kichThuocId;
-    }
-
-    public void setKichThuocId(Long kichThuocId) {
-        this.kichThuocId = kichThuocId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        KichThuocMauSac that = (KichThuocMauSac) o;
-        return id == that.id && soLuong == that.soLuong && trangThai == that.trangThai && Objects.equals(mauSacId, that.mauSacId) && Objects.equals(sanPhamId, that.sanPhamId) && Objects.equals(kichThuocId, that.kichThuocId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, soLuong, trangThai, mauSacId, sanPhamId, kichThuocId);
-    }
 }
--- src/main/java/com/poly/duanbangiay/entity/PhanQuyen.java
+++ src/main/java/com/poly/duanbangiay/entity/PhanQuyen.java
@@ -15,10 +15,6 @@ import java.util.Objects;
 @Getter
 @Setter
 public class PhanQuyen {
-
-    private Long taiKhoanId;
-    private Long quyenId;
-
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id")
@@ -38,48 +34,6 @@ public class PhanQuyen {
             referencedColumnName = "id",
             nullable = true
     )
-
     private Quyen quyen;
 
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "tai_khoan_id", nullable = true)
-    public Long getTaiKhoanId() {
-        return taiKhoanId;
-    }
-
-    public void setTaiKhoanId(Long taiKhoanId) {
-        this.taiKhoanId = taiKhoanId;
-    }
-
-    @Basic
-    @Column(name = "quyen_id", nullable = true)
-    public Long getRoleId() {
-        return quyenId;
-    }
-
-    public void setRoleId(Long roleId) {
-        this.quyenId = roleId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PhanQuyen phanQuyen = (PhanQuyen) o;
-        return id == phanQuyen.id && Objects.equals(taiKhoanId, phanQuyen.taiKhoanId) && Objects.equals(quyenId, phanQuyen.quyenId);
-    }
-    //
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, taiKhoanId, quyenId);
-    }
 }

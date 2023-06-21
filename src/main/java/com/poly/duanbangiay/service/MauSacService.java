e02491f2c27a0ba985ffe914d587498709c8c72b
quanluc2102 <quanluc2102@gmail.com>
2023-06-21 11:02:49 +0700

    thêm mausacservice và impl

--- src/main/java/com/poly/duanbangiay/service/MauSacService.java
+++ src/main/java/com/poly/duanbangiay/service/MauSacService.java
@@ -1,2 +1,17 @@
-package com.poly.duanbangiay.service;public class MauSacService {
+package com.poly.duanbangiay.service;
+
+
+import com.poly.duanbangiay.entity.MauSac;
+
+
+public interface MauSacService {
+    void add(MauSac mauSac);
+
+    void delete(Long id);
+
+    void update(MauSac mauSac);
+
+    MauSac detail(Long id);
+
+    MauSac findOne(Long id);
 }
--- src/main/java/com/poly/duanbangiay/service/serviceimpl/MauSacServiceImpl.java
+++ src/main/java/com/poly/duanbangiay/service/serviceimpl/MauSacServiceImpl.java
@@ -1,2 +1,43 @@
-package com.poly.duanbangiay.service.serviceimpl;public class MauSacServiceImpl {
+package com.poly.duanbangiay.service.serviceimpl;
+
+import com.poly.duanbangiay.entity.MauSac;
+import com.poly.duanbangiay.entity.MauSac;
+import com.poly.duanbangiay.repository.MauSacRepository;
+import com.poly.duanbangiay.service.MauSacService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MauSacServiceImpl implements MauSacService {
+
+    @Autowired
+    MauSacRepository mauSacRepository;
+
+    @Override
+    public void add(MauSac mauSac) {
+        mauSacRepository.save(mauSac);
+    }
+
+    @Override
+    public void delete(Long id) {
+        mauSacRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(MauSac mauSac) {
+        MauSac mauSac1 = mauSacRepository.findById(mauSac.getId()).get();
+        mauSac1.setGiaTri(mauSac.getGiaTri());
+        mauSac1.setTen(mauSac.getTen());
+        mauSac1.setTrangThai(mauSac.getTrangThai());
+        this.mauSacRepository.save(mauSac1);
+
+    }
+
+    @Override
+    public MauSac detail(Long id) {
+        return mauSacRepository.findById(id).get();
+    }
+
+    @Override
+    public MauSac findOne(Long id) {
+        return mauSacRepository.findById(id).get();
+    }
 }

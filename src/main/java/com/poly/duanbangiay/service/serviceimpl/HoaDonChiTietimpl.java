b4b399e40cedaad8f2a63d3d0383f657e66409e3
HUNG\buiti <hungbtph21019@fpt.edu.vn>
2023-06-29 12:01:54 +0700

    update

--- src/main/java/com/poly/duanbangiay/service/serviceimpl/HoaDonChiTietimpl.java
+++ src/main/java/com/poly/duanbangiay/service/serviceimpl/HoaDonChiTietimpl.java
@@ -1,2 +1,21 @@
-package com.poly.duanbangiay.service.serviceimpl;public class HoaDonChiTietimpl {
+package com.poly.duanbangiay.service.serviceimpl;
+
+import com.poly.duanbangiay.entity.HoaDonChiTiet;
+import com.poly.duanbangiay.repository.HoaDonChiTietRepository;
+import com.poly.duanbangiay.service.HoaDonChiTietService;
+import com.poly.duanbangiay.service.HoaDonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+@Service
+public class HoaDonChiTietimpl implements HoaDonChiTietService {
+    @Autowired
+    HoaDonChiTietRepository hoaDonChiTietRepository;
+
+    @Override
+    public List<HoaDonChiTiet> getAllByIdHD(Long idHD) {
+        return hoaDonChiTietRepository.findAllByHd_Id(idHD);
+    }
 }
--- src/main/java/com/poly/duanbangiay/service/serviceimpl/HoaDonImpl.java
+++ src/main/java/com/poly/duanbangiay/service/serviceimpl/HoaDonImpl.java
@@ -7,14 +7,20 @@ import com.poly.duanbangiay.service.HoaDonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
+
 @Service
 public class HoaDonImpl implements HoaDonService {
     @Autowired
     HoaDonRepository hoaDonRepository;
-    @Override
-    public void add(HoaDonService sp) {
 
+    @Override
+    public void add(HoaDon sp) {
+        sp.setNgayTao(Date.valueOf(LocalDate.now()));
+        sp.setNgayCapNhat(Date.valueOf(LocalDate.now()));
+        hoaDonRepository.save(sp);
     }
 
     @Override
@@ -23,8 +29,13 @@ public class HoaDonImpl implements HoaDonService {
     }
 
     @Override
-    public void update(Long id, HoaDonService hoaDon) {
-
+    public void update(HoaDon hoaDon) {
+        HoaDon hoaDon1 = hoaDonRepository.findById(hoaDon.getId()).get();
+        hoaDon1.setThanhToan(hoaDon.getThanhToan());
+        hoaDon1.setTaiKhoan(hoaDon.getTaiKhoan());
+        hoaDon1.setGhiChu(hoaDon.getGhiChu());
+        hoaDon1.setNgayCapNhat(Date.valueOf(LocalDate.now()));
+        hoaDonRepository.save(hoaDon1);
     }
 
     @Override
@@ -33,8 +44,8 @@ public class HoaDonImpl implements HoaDonService {
     }
 
     @Override
-    public SanPham getOne(Long id) {
-        return null;
+    public HoaDon getOne(Long id) {
+        return hoaDonRepository.findById(id).get();
     }
 
     @Override
